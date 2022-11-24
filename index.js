515009f5adacf9faad446c020c9bcf0a0d519a44
Nahid hassan <nahidlikhon22@gmail.com>
2022-11-25 03:46:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port=process.env.PORT || 5000;
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('your server is runing')
+})
+
+app.listen(port,()=>{
+    console.log('kenakata server is running')
+})
