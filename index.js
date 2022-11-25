4fd9b3ac5395db7fcd7a4cf26bf1f41bf1204692
Nahid hassan <nahidlikhon22@gmail.com>
2022-11-25 11:45:24 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express=require('express');
 const cors=require('cors');
 const app=express();
 const port=process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
+//midelewere
+app.use(cors());
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5gquyue.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
