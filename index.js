c5f13b01e9993aedf1eec9482015fdc2c9a2b6db
Nahid hassan <nahidlikhon22@gmail.com>
2022-11-28 03:27:33 +0600

    category function added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5gquyue.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,6 +22,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection=client.db('kenakata').collection('products')
+        const allCategoriy=client.db('kenakata').collection('categories')
         
         app.get('/products',async(req,res)=>{
             const query={}
@@ -47,7 +48,15 @@ async function run(){
             res.send(category)
         })
 
+        app.get('/category', async(req,res)=>{
+            const query={}
+            const cursor=allCategoriy.find(query)
+            const categories=await cursor.toArray()
+            res.send(categories)
 
+        })
+
+       
       
         
       
